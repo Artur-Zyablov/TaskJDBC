19effd3859d89d1fbf488901eb401a7f9db1e567
Charnel-DevOPS <Charnelside@gmail.com>
2021-08-19 17:08:21 +0300

    Update UserDaoJDBCImpl.java

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,4 +1,3 @@
-
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
@@ -9,121 +8,82 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+//Здесь используем Try with resources для autoclosable всех потоков и отлавливаем возможные исключения
+// получаемые в результате ошибки в SQL запросе либо при отсутствии Util класса
 
-    private Connection connection;
-    private Statement statement;
-    private PreparedStatement preparedStatement;
 
-    public UserDaoJDBCImpl() {
-        try {
-            connection = Util.getMySQLConnection();
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+    public UserDaoJDBCImpl() {}
 
     @Override
     public void createUsersTable() {
-        try {
-            connection = Util.getMySQLConnection();
-            statement = connection.createStatement();
+        try (Connection connection = Util.getMySQLConnection(); Statement statement = connection.createStatement()) {
             statement.execute("USE jdbc");
             statement.execute("CREATE TABLE IF NOT EXISTS users " +
                     "(id int PRIMARY KEY AUTO_INCREMENT UNIQUE, " +
                     "name VARCHAR(20) NOT NULL ," +
                     " lastName VARCHAR(20) NOT NULL ," +
                     " age int(3) NOT NULL)");
-            if(connection != null){
-                try{
-                    connection.commit();
-                }catch (Exception e){
-                    connection.rollback();
-                    System.out.println(e.getMessage());
-                }
-            }
             System.out.println("Таблица созданна");
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Таблица не создана");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Потеряли Util.getMySQLCon");
         }
     }
 
     @Override
     public void dropUsersTable() {
-        try {
-            connection = Util.getMySQLConnection();
-            connection.setAutoCommit(false);
-            statement = connection.createStatement();
-            statement.execute("DROP TABLE users");
+        try (Connection conn = Util.getMySQLConnection(); Statement st = conn.createStatement()) {
+            st.execute("DROP TABLE users");
             System.out.println("Таблица удаленна");
-            if(connection != null){
-                try{
-                    connection.commit();
-                }catch (Exception e){
-                    connection.rollback();
-                    System.out.println(e.getMessage());
-                }
-            }
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
+            conn.commit();
+        } catch (SQLException e) {
+            System.out.println("Таблица не удалена");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Потеряли Util.getMySQLCon");
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try {
-            String sql = "INSERT INTO users (name, lastName, age) Values (?, ?, ?)";
-            connection = Util.getMySQLConnection();
-            connection.setAutoCommit(false);
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
+        try (Connection conn = Util.getMySQLConnection();
+             PreparedStatement ps = conn
+                     .prepareStatement("INSERT INTO users (name, lastName, age) Values (?, ?, ?)")) {
+            ps.setString(1, name);
+            ps.setString(2, lastName);
+            ps.setByte(3, age);
+            ps.executeUpdate();
             System.out.println("User с именем – " + name + " добавлен в базу данных");
-            if(connection != null){
-                try{
-                    connection.commit();
-                }catch (Exception e){
-                    connection.rollback();
-                    System.out.println(e.getMessage());
-                }
-            }
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
+            conn.commit();
+        } catch (SQLException e) {
+            System.out.println("Юзер не добавлен");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Потеряли Util.getMySQLCon");
         }
     }
 
     @Override
     public void removeUserById(long id) {
-        try {
-            String sql = "DELETE FROM users WHERE id = ?;";
-            connection = Util.getMySQLConnection();
-            connection.setAutoCommit(false);
-            preparedStatement = connection.prepareStatement(sql);
+        try (Connection conn = Util.getMySQLConnection();
+             PreparedStatement preparedStatement =
+                     conn.prepareStatement("DELETE FROM users WHERE id = ?;")) {
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
-            if(connection != null){
-                try{
-                    connection.commit();
-                }catch (Exception e){
-                    connection.rollback();
-                    System.out.println(e.getMessage());
-                }
-            }
+            conn.commit();
             System.out.println("User с id - " + id + " удален из таблицы");
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Юзер не удален");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Потеряли Util.getMySQLCon");
         }
     }
 
     @Override
     public List<User> getAllUsers() {
         List<User> allUsers = new ArrayList<>();
-        try {
-            String sql = "SELECT * FROM users";
-            preparedStatement = connection.prepareStatement(sql);
-            connection.setAutoCommit(false);
-            ResultSet rs = preparedStatement.executeQuery();
+        try (Connection conn = Util.getMySQLConnection(); PreparedStatement ps = conn.
+                prepareStatement("SELECT * FROM users");
+             ResultSet rs = ps.executeQuery()) {
             while (rs.next()) {
                 User user = new User();
                 user.setId(rs.getLong("id"));
@@ -132,38 +92,25 @@ public class UserDaoJDBCImpl implements UserDao {
                 user.setAge(rs.getByte("age"));
                 allUsers.add(user);
             }
-            if(connection != null){
-                try{
-                    connection.commit();
-                }catch (Exception e){
-                    connection.rollback();
-                    System.out.println(e.getMessage());
-                }
-            }
+            conn.commit();
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Не выполнился sql запрос на вывод юзеров из таблицы");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Потеряли Util.getMySQLCon");
         }
         return allUsers;
     }
 
     @Override
     public void cleanUsersTable() {
-        try {
-            connection = Util.getMySQLConnection();
-            connection.setAutoCommit(false);
-            statement = connection.createStatement();
-            statement.execute("DELETE FROM users");
+        try (Connection conn = Util.getMySQLConnection(); Statement st = conn.createStatement()) {
+            st.execute("DELETE FROM users");
             System.out.println("Таблица очищена");
-            if(connection != null){
-                try{
-                    connection.commit();
-                }catch (Exception e){
-                    connection.rollback();
-                    System.out.println(e.getMessage());
-                }
-            }
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
+            conn.commit();
+        } catch (SQLException e) {
+            System.out.println("Таблица не очищена");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Потеряли Util.getMySQLCon");
         }
     }
 }
